ffd57a4a042bebdc76664b3de7a9b9f6375b5720
musyokamuasya <josephmuasya8@gmail.com>
2022-03-11 23:19:03 +0300

    Separate the function from the main class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,7 @@
+import basics.Functions
+
 fun main(args: Array<String>) {
-printSum(19.2, 3.1)
-   
+   val functions = Functions()
+    functions.printSum(18.2, 6.7)
 }
 
-fun printSum (firstNumber: Double, secondNumber: Double){
-    val sum = firstNumber + secondNumber
-    println("The sum of $firstNumber and $secondNumber is $sum")
-}
--- src/main/kotlin/basics/Functions.kt
+++ src/main/kotlin/basics/Functions.kt
@@ -1,4 +1,8 @@
 package basics
 
 class Functions {
+    fun printSum (firstNumber: Double, secondNumber: Double){
+        val sum = firstNumber + secondNumber
+        println("The sum of $firstNumber and $secondNumber is $sum")
+    }
 }
