de5b37dd89374eaf127a54590a2742fac5b5c489
musyokamuasya <josephmuasya8@gmail.com>
2022-03-11 23:07:01 +0300

    Basic Kotlin function

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+printSum(19.2, 3.1)
+   
+}
+
+fun printSum (firstNumber: Double, secondNumber: Double){
+    val sum = firstNumber + secondNumber
+    println("The sum of $firstNumber and $secondNumber is $sum")
+}
