677afc03183b036fbb0f307304aabc6692a90d1c
musyokamuasya <josephmuasya8@gmail.com>
2022-03-14 01:14:10 +0300

    Pass string templates in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,5 +3,6 @@ import basics.Functions
 fun main(args: Array<String>) {
    val functions = Functions()
     functions.printSum(18.2, 6.7)
+    println(functions.stringTemplates())
 }
 
--- src/main/kotlin/basics/Functions.kt
+++ src/main/kotlin/basics/Functions.kt
@@ -5,4 +5,14 @@ class Functions {
         val sum = firstNumber + secondNumber
         println("The sum of $firstNumber and $secondNumber is $sum")
     }
+
+    fun stringTemplates() {
+        var a = 1;
+        val s1 = "a is $a"
+
+        a = 2;
+
+        val s2 = "${s1.replace("is", "was")} but now is $a"
+
+    }
 }
